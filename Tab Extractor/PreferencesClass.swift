81fe69169b1673e53feb94338d1852a569174459
Ionut Sava <object.inspector@gmail.com>
2022-01-07 09:00:29 +0200

    separated ui

--- Tab Extractor/PreferencesClass.swift
+++ Tab Extractor/PreferencesClass.swift
@@ -11,10 +11,14 @@ import SwiftUI
 enum GlobalPreferences: String {
     case viewSourceLines
     static let viewSourceLinesDefault = false
-    case includeBars
-    static let includeBarsDefault = true
+    
+    case includeHeader
+    static let includeHeaderDefault = true
     case stringStringSeparator
     static let stringStringSepparatorDefault = " "
+    case includeBars
+    static let includeBarsDefault = true
+    
     case stringNoteSeparator
     static let stringNoteSeparatorDefault: String = ""
     case noteNoteSeparator
@@ -22,5 +26,27 @@ enum GlobalPreferences: String {
     
     case viewHorizontally
     static let viewHorizontallyDefault = true
+    
+    case noStrings6
+    static let noStrings6Default = true
+    case noStrings6Names
+    static let noString6NamesDefault = "e, B, G, D, A, E"
+    case noStrings4
+    static let noStrings4Default = true
+    case noStrings4Names
+    static let noString4NamesDefault = "G, D, A, E"
+    case noStrings7
+    static let noStrings7Default = true
+    case noStrings7Names
+    static let noString7NamesDefault = "e, b, G, D, A, E, B"
+    
+    static let nsn6 = Pref2(defaultValue: true)
 }
 typealias GLBP = GlobalPreferences
+
+struct Pref2<ValueType> {
+    let defaultValue: ValueType
+    var settingName: String {
+        return String(describing: self)
+    }
+}
--- Tab Extractor/views/PreferencesView.swift
+++ Tab Extractor/views/PreferencesView.swift
@@ -12,19 +12,27 @@ struct InnerPreferencesView: View {
     
     @AppStorage( wrappedValue: GLBP.viewSourceLinesDefault, GLBP.viewSourceLines.rawValue) var viewSourceLines
     
-    @AppStorage( wrappedValue: GLBP.includeBarsDefault, GLBP.includeBars.rawValue) private var includeBars
+    @AppStorage( wrappedValue: GLBP.includeHeaderDefault, GLBP.includeHeader.rawValue) private var includeHeader
     @AppStorage( wrappedValue: GLBP.stringStringSepparatorDefault, GLBP.stringStringSeparator.rawValue) var stringStringSeparator
+    @AppStorage( wrappedValue: GLBP.includeBarsDefault, GLBP.includeBars.rawValue) private var includeBars
+    
     @AppStorage( wrappedValue: GLBP.stringNoteSeparatorDefault, GLBP.stringNoteSeparator.rawValue) var stringNoteSeparator
     @AppStorage( wrappedValue: GLBP.noteNoteSeparatorDefault, GLBP.noteNoteSeparator.rawValue) var noteNoteSeparator
     
     @AppStorage( wrappedValue: GLBP.viewHorizontallyDefault, GLBP.viewHorizontally.rawValue) private var viewHorizontally
+    
+    @AppStorage( wrappedValue: GLBP.noStrings6Default, GLBP.noStrings6.rawValue) var noStrings6
+    @AppStorage( wrappedValue: GLBP.noString6NamesDefault, GLBP.noStrings6Names.rawValue) var noStrings6Names
+    @AppStorage( wrappedValue: GLBP.noStrings4Default, GLBP.noStrings4.rawValue) var noStrings4
+    @AppStorage( wrappedValue: GLBP.noString4NamesDefault, GLBP.noStrings4Names.rawValue) var noStrings4Names
+    @AppStorage( wrappedValue: GLBP.noStrings7Default, GLBP.noStrings7.rawValue) var noStrings7
+    @AppStorage( wrappedValue: GLBP.noString7NamesDefault, GLBP.noStrings7Names.rawValue) var noStrings7Names
     //@Environment(\.accessibilityDifferentiateWithoutColor) var diffWithoutColor
     
     var body: some View {
         Form {
             Section(header: Text("How to display tabs")) {
             AccessibleCheckBox(checked: $viewSourceLines, caption: "Display tabs as original, unprocessed, lines")
-                //Toggle("Display tabs as original, unprocessed, lines", isOn: self.$viewSourceLines)
             } //se
             if !viewSourceLines {
             Picker("Display each tab as", selection: $viewHorizontally) {
@@ -34,25 +42,25 @@ struct InnerPreferencesView: View {
                     .tag(false)
             } //pk
             .pickerStyle(InlinePickerStyle() )
-            .disabled( self.viewSourceLines)
+            //.disabled( self.viewSourceLines)
             Section(header: Text("Options for processed tabs:")) {
+                HStack {
+                Toggle("Include header line (the string names at the beginning of the tab)", isOn: self.$includeHeader)
+                    Spacer()
+                } //hs
+                    .padding(.top)
+                HStack {
+                    Text(String(format: "Separator between string names (E%1$@A%1$@D%1$@G...)", self.stringStringSeparator))
+                    Spacer()
+                    TextField("", text: self.$stringStringSeparator)
+                    .limitToMaxWidth(geo: geo, ratio: 0.25)
+                } //hs
+                .padding(.vertical)
                 HStack {
                 Toggle("Include bars when listing tab content", isOn: self.$includeBars)
                     Spacer()
                 } //hs
-                    .padding(.vertical)
-                HStack {
-                    //Text("Separator between string names (E A D G...)")
-                    Text(String(format: "Separator between string names (E%1$@A%1$@D%1$@G...)", self.stringStringSeparator))
-                    Spacer()
-                    TextField("", text: self.$stringStringSeparator) { isEditing in
-                        //
-                    } onCommit: {
-                        //
-                    } //tf
-                    .limitToMaxWidth(geo: geo, ratio: 0.25)
-                } //hs
-                .padding(.vertical)
+                    .padding(.top)
                 VStack {
                 HStack {
                     Text("Separator between string name and note")
@@ -75,6 +83,20 @@ struct InnerPreferencesView: View {
                 .padding(.vertical)
                 Text("Tip: use dots, commas or semicolons to help VoiceOver make longer or shorter pauses.")
             } //sect
+                Section("When string names are missing") {
+                    VStack {
+                    Toggle("6 strings default", isOn: $noStrings6)
+                    TextField(GLBP.noString6NamesDefault, text: $noStrings6Names)
+                }.padding(.vertical)
+                    VStack {
+                    Toggle("4 strings default (bass)", isOn: $noStrings4)
+                    TextField(GLBP.noString4NamesDefault, text: $noStrings4Names)
+                }.padding(.vertical)
+                    VStack {
+                    Toggle("7 strings default", isOn: $noStrings7)
+                    TextField(GLBP.noString7NamesDefault, text: $noStrings7Names)
+                }.padding(.vertical)
+                } //se
             } //if processed
         } //fo
         .padding(.bottom)
