961ab8e799aea9144bf7d2c7fac65649e44eaf71
Ionut Sava <object.inspector@gmail.com>
2022-02-08 14:55:42 +0200

    added icons

--- Tab Extractor/Tab_ExtractorApp.swift
+++ Tab Extractor/Tab_ExtractorApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct Tab_ExtractorApp: App {
+    @UIApplicationDelegateAdaptor(TEAppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+class TEAppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Thread.sleep(forTimeInterval: 3)
+        return true
+    }
+} //cl
